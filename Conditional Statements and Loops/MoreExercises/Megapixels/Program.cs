dac9e2edcc49fd5027fd05f37ecd41ab8a99cee8
rpopo <rados270593@gmail.com>
2017-09-28 13:54:24 +0300

    addExercise

---
+++ Conditional Statements and Loops/MoreExercises/Megapixels/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megapixels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double counting = width * height;
+            double megapix = counting / 1000000;
+            megapix = double.Parse(megapix.ToString("0.0"));
+            Console.WriteLine(megapix);
+        }
+    }
+}
