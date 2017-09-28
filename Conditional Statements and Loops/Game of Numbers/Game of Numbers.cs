e1a7b87b6b7304ea20cadff2485cc27aa31c338f
rpopo <rados270593@gmail.com>
2017-09-28 10:17:44 +0300

    HomeWorkFundamental

---
+++ Conditional Statements and Loops/Game of Numbers/Game of Numbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int terminated = int.Parse(Console.ReadLine());
+            int magicDigit = int.Parse(Console.ReadLine());
+            int currentCombination = 0;
+            int sum = 0;
+            int curG = 0;
+            int curI = 0;
+            for (int g = start; g <= terminated; g++)
+            {
+                for (int i = start; i <= terminated; i++)
+                {
+                    currentCombination++;
+                    sum = g + i;
+                    if (sum == magicDigit)
+                    {
+                        curG = g;
+                        curI = i;
+                    }
+                }
+            }
+            if (curG != 0)
+            {
+                Console.WriteLine($"Number found! {curG} + {curI} = {magicDigit}");
+            }
+            else
+            {
+                Console.WriteLine($"{currentCombination} combinations - neither equals {magicDigit}");
+            }
+        }
+    }
+}
---
+++ Conditional Statements and Loops/Interval of Numbers/Interval of Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interval_of_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            PrintSequanceNumbers(start, end);
+        }
+
+        static void PrintSequanceNumbers(int start, int end)
+        {
+            int bigest = Math.Max(start, end);
+            int lowest = Math.Min(start, end);
+            for (int i = lowest; i <= bigest; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
