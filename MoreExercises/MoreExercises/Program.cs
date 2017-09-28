bce380581cfa8e3765060c5b976106bf2066141d
rpopo <rados270593@gmail.com>
2017-09-28 11:41:05 +0300

    UploadingExercise

---
+++ MoreExercises/MoreExercises/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char x = 'x';
+            char whiteSpace = ' ';
+            string format = "{0}{1}{2}{1}{0}";
+            for (int i = 0; i < n / 2; i++)
+            {
+                string top = string.Format(format,
+                    new string(whiteSpace, i),
+                        new string(x, 1),
+                            new string(whiteSpace, (n - 2) - i * 2),
+                                new string(x, 1),
+                                    new string(whiteSpace, i));
+                Console.WriteLine(top);
+            }
+            string midFormat = "{0}{1}{0}";
+            Console.WriteLine(string.Format(midFormat,
+                new string(whiteSpace, n / 2),
+                    new string(x, 1)));
+            for (int i = 1; i <=n/2; i++)
+            {
+                string mid = string.Format(format,
+                     new string(whiteSpace, n / 2 - i),
+                        new string(x, 1),
+                            new string(whiteSpace, i*2-1));
+                Console.WriteLine(mid);
+            }
+        }
+    }
+}
---
+++ MoreExercises/Vapor Store/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vapor_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int balance = int.Parse(Console.ReadLine());
+            string order = Console.ReadLine();
+            Console.WriteLine(
+            PrintAmountMoney(order));
+            
+            
+
+        double PrintAmountMoney(string Ordered)
+        {
+                double OutFall4 = 39.99;
+                double CSOG = 15.99;
+                double Zplinter = 19.99;
+                double Honored2 = 59.99;
+                double RoverWatch = 29.99;
+                double RoverWatchORG = 39.99;
+                double amount = 0;
+                while (order != "Game Time")
+                {
+
+                    if (order == "OutFall 4")
+                    {
+                        amount += balance - OutFall4;
+                    }
+                    else if (order == "CS: OG")
+                    {
+                        amount += balance - CSOG;
+                    }
+                    else if (order == "Zplinter Zell")
+                    {
+                        amount += balance - Zplinter;
+                    }
+                    else if (order == "Honored 2")
+                    {
+                        amount += balance - Honored2;
+                    }
+                    else if (order == "RoverWatch")
+                    {
+                        amount += balance - RoverWatch;
+                    }
+                    else if (order == "RoverWatch Origins Edition")
+                    {
+                        amount += balance - RoverWatchORG;
+                    }
+                    order = Console.ReadLine();
+                }
+                return amount;
+            }
+
+        }
+    }
+}
